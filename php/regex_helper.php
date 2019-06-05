3623e99e58270a9a30c9b14c3748ccd25481aa42
Marcel Maci <m.maci@gmx.ch>
2019-06-05 19:32:37 +0200

    statistics for rule generator ... we are at an impressive 44892 token combinations (and counting ...)

--- php/regex_helper.php
+++ php/regex_helper.php
@@ -18,6 +18,9 @@ function GetRegexOrString($array) {
     return $temp1;
 }
 
+// permutations
+$permutations = array();
+
 // define data
 require_once "regex_helper_import.php"; // $token_groups array
 $token_variants = array(); // nice side-effect: not necessary since token_combiner copies offset 23 of token header (and hence the group!)
@@ -47,30 +50,30 @@ $vowel_groups = array(
 // rules: combination + vowel + distance (string) + mandatory/optional
 // for each combination (2 tokens out of groups) a vowel group can be given a specific distance
 $rules_list = array( 
-    "R1" => array( "C1", "V1", "D1", "" ), // "?" means optional (used as regex token)
-    "R2" => array( "C1", "V2", "D1", "?" ),   // "" means mandatory
-    "R3" => array( "C2", "V1", "D3", "" ),
-    "R4" => array( "C2", "V2", "D4", "?" ),
-    "R5" => array( "C3", "V1", "D5", "" ),
-    "R6" => array( "C3", "V2", "D6", "?" ),
-    "R7" => array( "C4", "V1", "D7", "" ),
-    "R8" => array( "C4", "V2", "D7", "?" ),
-    "R9" => array( "C5", "V1", "D8", "" ),
-    "R10" => array( "C5", "V2", "D9", "?" ),
-    "R11" => array( "C6", "V1", "D10", "" ),
-    "R12" => array( "C6", "V2", "D11", "?" ),
-    "R13" => array( "C7", "V1", "D12", "" ),
-    "R14" => array( "C7", "V2", "D14", "?" ),
-    "R15" => array( "C8", "V1", "D15", "" ),
-    "R16" => array( "C8", "V2", "D16", "?" ),
-    "R17" => array( "C9", "V1", "D17", "" ),
-    "R18" => array( "C9", "V2", "D18", "?" ),
-    "R19" => array( "C10", "V1", "D19", "" ),
-    "R20" => array( "C10", "V2", "D20", "?" ),
-    "R21" => array( "C11", "V1", "D21", "" ),
-    "R22" => array( "C11", "V2", "D22", "?" ),
-    "R23" => array( "C12", "V1", "D23", "" ),
-    "R24" => array( "C12", "V2", "D24", "?" ),
+    "R1" => array( "C1", "V1", "D1", "?" ), // "?" means optional (used as regex token)
+    "R2" => array( "C1", "V2", "D2", "" ),   // "" means mandatory
+    "R3" => array( "C2", "V1", "D3", "?" ),
+    "R4" => array( "C2", "V2", "D4", "" ),
+    "R5" => array( "C3", "V1", "D5", "?" ),
+    "R6" => array( "C3", "V2", "D6", "" ),
+    "R7" => array( "C4", "V1", "D7", "?" ),
+    "R8" => array( "C4", "V2", "D7", "" ),
+    "R9" => array( "C5", "V1", "D8", "?" ),
+    "R10" => array( "C5", "V2", "D9", "" ),
+    "R11" => array( "C6", "V1", "D10", "?" ),
+    "R12" => array( "C6", "V2", "D11", "" ),
+    "R13" => array( "C7", "V1", "D12", "?" ),
+    "R14" => array( "C7", "V2", "D14", "" ),
+    "R15" => array( "C8", "V1", "D15", "?" ),
+    "R16" => array( "C8", "V2", "D16", "" ),
+    "R17" => array( "C9", "V1", "D17", "?" ),
+    "R18" => array( "C9", "V2", "D18", "" ),
+    "R19" => array( "C10", "V1", "D19", "?" ),
+    "R20" => array( "C10", "V2", "D20", "" ),
+    "R21" => array( "C11", "V1", "D21", "?" ),
+    "R22" => array( "C11", "V2", "D22", "" ),
+    "R23" => array( "C12", "V1", "D23", "?" ),
+    "R24" => array( "C12", "V2", "D24", "" ),
      
 );
 
@@ -129,6 +132,8 @@ foreach ($token_groups as $key => $tokens) {
     $temp2 = GetRegexOrString($token_variants[$key]);
     // write full regex and insert (use non capturing groups ?:)
     $token_groups_string[$key] = (mb_strlen($temp2) === 0) ? "\[(?:$temp1)\]" : "\[(?:$temp1)(?:$temp2)?\]";
+    // calculate permutations
+    $permutations[$key] = count($tokens) * (count($token_variants[$key])+1);
 }
 
 // vowels
@@ -139,13 +144,16 @@ foreach ($vowel_groups as $key => $vowels) {
     $temp1 = GetRegexOrString($vowels);
     // write full regex and insert
     $vowel_groups_string[$key] = "\[(?:$temp1)\]";
+    // calculate permutations
+    $permutations[$key] = count($vowels);
 }
 
 // show result
-echo "<h2>TOKENS</h2>"; foreach ($token_groups_string as $key => $string) echo "$key: $string<br>";
-echo "<h2>VOWELS</h2>"; foreach ($vowel_groups_string as $key => $string) echo "$key: $string<br>";
+echo "<h2>TOKENS</h2>"; foreach ($token_groups_string as $key => $string) echo "$key: $string (" . $permutations[$key]. ")<br>";
+echo "<h2>VOWELS</h2>"; foreach ($vowel_groups_string as $key => $string) echo "$key: $string (" . $permutations[$key]. ")<br>";
 
 // generate rules
+$total_permutations = 0;
 $rules_list_string = array();
 foreach ($rules_list as $key => $rule) {
     // get combinations
@@ -154,13 +162,19 @@ foreach ($rules_list as $key => $rule) {
     $group1 = $token_groups_string[$group1_key];
     $group2 = $token_groups_string[$group2_key];
     $vowel = $vowel_groups_string[$vowel_key];
+    // calculate permutations
+    $new_permutations = $permutations[$group1_key] * ($permutations[$vowel_key] + (($optional === "?") ? 1 : 0)) * $permutations[$group2_key];
+    $total_permutations += $new_permutations;
     // write rule and insert
-    $rules_list_string[$key] = mb_strtolower("\"($group1)($vowel)$optional($group2)\" => \"$1[#$distance]$2$3\";") . " // $key|$combination_key: $group1_key#$vowel_key:$distance#$group2_key";
-}
+    $rules_list_string[$key] = mb_strtolower("\"($group1)($vowel)$optional($group2)\" => \"$1[#$distance]$2$3\";") . " // $key|$combination_key: $group1_key#$vowel_key:$distance#$group2_key ($new_permutations)";
+}   
 
 // show result (copy it to VSTENO model)
-echo "<h2>RULES</h2>"; foreach ($rules_list_string as $key => $string) echo "$string<br>";
+echo "<h2>RULES</h2><p>// statistics: these rules cover approximately <b>$total_permutations</b> token combinations.</p>"; foreach ($rules_list_string as $key => $string) echo "$string<br>";
 
+// statistics
+echo "<h2>STATISTICS</h2>"; 
+echo "<p>These rules cover approximately <b>$total_permutations</b> token combinations.</p>";
 
 ?>
   </body>
