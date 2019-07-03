3f576249a8ebf7c2b7bb65cbcc993205839f48ba
Marcel Maci <m.maci@gmx.ch>
2019-07-03 12:56:58 +0200

    improvements french model

--- php/constants.php
+++ php/constants.php
@@ -25,8 +25,8 @@ ini_set('display_errors','off');    // turn off errors in order to keep error.lo
 error_reporting(0);                 // turn off all error reporting
 
 // version
-const version_commit_id = "7b1c97fd98d780ab48e1a812aaa74e7f81f72f72";   // must be inserted manually after commit => could be from last commit if forgotten
-const version_date = "1. Juli 2019";                                  // idem
+const version_commit_id = "37f9d86132d49545e968e509036f932aae58ee3a";   // must be inserted manually after commit => could be from last commit if forgotten
+const version_date = "2. Juli 2019";                                  // idem
 
 // constants
 // for steno tokens (array with header and data tuplets)
