1808995a990a14e3c1b7d70f862f9e1ce20778f0
smuege <m.maci@gmx.ch>
2020-05-11 10:49:20 +0200

    improvements DESSBAS (special rule project Gutenberg), publication (The Taming of the Shrew, version Karl Ettlinger)

--- php/library.php
+++ php/library.php
@@ -3,13 +3,15 @@
 <img src="../web/bookshelf_horizontal2_scaled850.png" style="width:830px;">
 <i>Auf dieser Seite finden Sie Lektüre in Stolze-Schrey. Bitte beachten Sie die Hinweise zu <a href="library_copyright.php">Copyright</a>, 
 <a href="library_philosophy.php">Publikations&shy;philosophie</a> und zur Abgeltung 
-von <a href="library_royalties.php">Lizenzgebühren</a>. Foto: (c) <a href="https://www.flickr.com/photos/scosanf/">Scott Sanford</a> (CC BY-NC-SA 2.0).</i>
+von <a href="license_fees.php">Lizenzgebühren</a>. Foto: (c) <a href="https://www.flickr.com/photos/scosanf/">Scott Sanford</a> (CC BY-NC-SA 2.0).</i>
 
 <p><h1>eBooks</h1></p>
 
 <ul>
     <li>Franz Kafka, Ein Bericht für eine Akademie: <a href="../books/franz_kafka_ein_bericht_fuer_eine_akademie_27042020.pdf">PDF 27/04/2020</a> 
     (<a href="http://www.gutenberg.org/files/21989/21989-0.txt">Original</a>)</li>
+    <li>Karl Ettlinger, Der Widerspenstigen Zähmung: <a href="../books/karl_ettlinger_der_widerspenstigen_zaehmung_11052020B.pdf">PDF 11/05/2020</a>
+    (<a href="http://www.gutenberg.org/cache/epub/31733/pg31733.txt">Original</a>)</li>
 </ul>
 
 <?php require "vsteno_template_bottom.php"; ?>
