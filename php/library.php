2f3d8648c653ea6c42c863f13c5361b262181a11
smuege <m.maci@gmx.ch>
2020-05-24 10:25:07 +0200

    add Symposion by Plato and information about proofreading

--- php/library.php
+++ php/library.php
@@ -2,16 +2,18 @@
 <h1>Bibliothek</h1>
 <img src="../web/bookshelf_horizontal2_scaled850.png" style="width:830px;">
 <i>Auf dieser Seite finden Sie Lektüre in Stolze-Schrey. Bitte beachten Sie die Hinweise zu <a href="library_copyright.php">Urheberrecht</a>, 
-<a href="library_philosophy.php">Publikations&shy;philosophie</a> und zur Abgeltung 
+<a href="library_philosophy.php">Publikations&shy;philosophie</a>, <a href="library_proofreading.php">Lektorat</a> und zur Abgeltung 
 von <a href="license_fees.php">Lizenzgebühren</a>. Foto: (c) <a href="https://www.flickr.com/photos/scosanf/">Scott Sanford</a> (CC BY-NC-SA 2.0).</i>
 
 <p><h1>eBooks</h1></p>
 
 <ul>
-    <li>Franz Kafka, Ein Bericht für eine Akademie: <a href="../books/franz_kafka_ein_bericht_fuer_eine_akademie_19052020.pdf">PDF 19/05/2020</a> 
-    (<a href="http://www.gutenberg.org/files/21989/21989-0.txt">Original</a>) [ältere Version: <a href="../books/franz_kafka_ein_bericht_fuer_eine_akademie_27042020.pdf">PDF 27/04/2020</a>]</li>
-    <li>Karl Ettlinger, Der Widerspenstigen Zähmung: <a href="../books/karl_ettlinger_der_widerspenstigen_zaehmung_16052020.pdf">PDF 16/05/2020</a>
+    <li>Franz Kafka, Ein Bericht für eine Akademie (20 Seiten): <a href="../books/franz_kafka_ein_bericht_fuer_eine_akademie_19052020.pdf">PDF 19/05/2020</a> 
+    (<a href="http://www.gutenberg.org/files/21989/21989-0.txt">Original</a>) [<a href="../books/franz_kafka_ein_bericht_fuer_eine_akademie_27042020.pdf">PDF 27/04/2020</a>]</li>
+    <li>Karl Ettlinger, Der Widerspenstigen Zähmung (231 Seiten): <a href="../books/karl_ettlinger_der_widerspenstigen_zaehmung_16052020.pdf">PDF 16/05/2020</a>
     (<a href="http://www.gutenberg.org/cache/epub/31733/pg31733.txt">Original</a>)</li>
+    <li>Platon, Gastmahl (96 Seiten): <a href="../books/platon_gastmahl_24052020.pdf">PDF 24/05/2020</a>
+    (<a href="http://www.gutenberg.org/files/24899/24899-0.txt">Original</a>)</li>
 </ul>
 
 <?php require "vsteno_template_bottom.php"; ?>
---
+++ php/library_proofreading.php
@@ -0,0 +1,18 @@
+<?php require "vsteno_template_top.php"; ?>
+<h1>Lektorat</h1>
+<p>Alle auf VSTENO publizierten eBooks werden nach einer der folgenden Methoden lektoriert:</p> 
+<ul>
+    <li><p><b>Korpus-Lektorierung:</b> Hier wird ausgehend vom Originaltext mithilfe des Programmes 
+    <a href="http://www.etymo.net/program/download/source/mkcor.cc">MKCOR</a> zuerst ein Wortkorpus erstellt, d.h. eine Wortliste, in der jedes 
+    Wort aus dem Originaltext nur ein Mal vorkommt. Diese Liste wird anschliessend übertragen und allfällige Fehler korrigiert. Danach 
+    wird der gesamte Text generiert und publiziert. Da der Wortkorpus nur zwischen einem Viertel und einem Achtel des Originaltextes ausmacht, 
+    können mit dieser Methode ganze Bücher sehr schnell lektoriert werden.</p></li>
+    <li><p><b>Volltext-Lektorierung:</b> Hier wird der gesamte Text übertragen und Korrektur gelesen. Diese Methode ist langsamer als die erste, da der 
+    gesamte Text gelesen werden muss. Der Vorteil besteht darin, dass auch Formatierungen und Wörter, die je nach Kontext anders geschrieben 
+    werden, überprüft werden können.</p></li>
+</ul>
+<p>Die beiden Methoden lassen sich auch kombinieren. Im Anhang der eBooks wird angegeben, nach welcher Methode die Texte lektoriert wurden. Falls 
+Sie weitere Fehler in den Büchern finden, so <a href="mailto:m.maci@gmx.ch">melden</a> Sie dies doch bitte. Beachten Sie bitte, dass 
+keine Garantie gegeben werden kann, dass die publizierten eBooks fehlerfrei sind.</p>
+
+<?php require "vsteno_template_bottom.php"; ?>
