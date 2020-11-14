678bcbb2352fe47813076f116aa892a72549b00d
smuege <m.maci@gmx.ch>
2020-11-14 09:11:54 +0100

    update links page

--- php/links.php
+++ php/links.php
@@ -11,7 +11,9 @@ zu diesen höchst spannenden Projekten unter <i>Artikel</i>.</p></li>
 </ul>
 <h2>Artikel</h2>
 <ul>
-<li><p><a href="https://www.dante.de/DTK/Ausgaben/dtk09-1.pdf">Text2DEK</a>: Wissenschaftlicher Artikel (Seiten 7-20) von Dr. Stanislav Jan Šarman zu Text2DEK.</p>
+<li><p><a href="https://www.dante.de/DTK/Ausgaben/dtk09-1.pdf">Text2DEK</a>: Wissenschaftlicher Artikel (Seiten 7-20) von Dr. Stanislav 
+Jan Šarman zu Text2DEK. Leider scheint der usprüngliche Link nicht mehr zu funktionieren: 
+<a href="https://archiv.dante.de/DTK/PDF/komoedie_2009_1.pdf">alternativer Link</a> (Archiv)</p>
 <li><p><a href="https://www.tug.org/TUGboat/tb29-3/tb93sarman.pdf">Text2Gregg</a>: Wissenschaftlicher Artikelvon Dr. Stanislav Jan Šarman zu Text2Gregg.</p>
 <li><p><a href="../docs/On-Line-Handschriftenerkennung-der-Deutschen-Einheitskurzschrift.pdf">OCR DEK</a>: "Online Handschriftenerkennung der Deutschen Einheitskurzschrift", Masterarbeit von Niko Will (2010).</li>
 </ul>
@@ -26,5 +28,14 @@ Hier finden Sie weiterführende Links zu Lehrmitteln, Kursen, Wettschreiben, reg
 <li><p><a href="http://www.stolze-schrey.de">Stenografenverband Stolze-Schrey Deutschland</a>: Hier finden Sie Hintergrundinformationen zu Heinrich 
 August Wilhelm Stolze und Ferdinand Schrey. Der Verband ist die offizielle Anlaufstelle für Interessierte in Deutschland und gibt regelmässig die "Stolze-Schrey-Post" heraus.</p>
 </ul>
+
+<h2>Lehrmittel</h2>
+<ul>
+<li><p><a href="https://steno.tu-clausthal.de/samples/WolterLehrbuch.pdf">Adolf Wolter (1909)</a>: Nachdruck des "Lehrbuchs der vereinfachten 
+Stenographie Einigunssystem Stolze-Schrey" von Adolf Wolter (1909), erstellt mit <a href="https://steno.tu-clausthal.de/Stolze-Schrey.php">Text2Stolze-Schrey</a> 
+durch Dr. Stanislav Jan Šarman und frei zugänglich.</p>
+<li><p><a href="http://www.steno.ch/0/index.php/lehrmittel">Lehrmittelshop SSV</a>: Offizielle Bezugsquelle des Schweizerischen Stenografenverbands SSV</p>
+</ul>
+
 
 <?php require "vsteno_template_bottom.php"; ?>
